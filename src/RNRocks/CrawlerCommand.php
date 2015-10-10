03ff261ad5dad43f834d0f78bb21dd0248f7e20a
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2015-10-10 18:56:08 +0200

    Implemented crawling logic.

--- src/RNRocks/CrawlerCommand.php
+++ src/RNRocks/CrawlerCommand.php
@@ -10,7 +10,10 @@
  */
 namespace RNRocks;
 
+use RNRocks\Repository\RepositoryFactory;
+use RNRocks\Repository\SculpinUsergroupRepository;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -22,7 +25,13 @@ class CrawlerCommand extends Command
     protected function configure()
     {
         $this->setName('crawler')
-            ->setDescription('Crawler to automate the event retrieval process');
+            ->setDescription('Crawler to automate the event retrieval process')
+            ->setDefinition(
+                array(
+                    new InputArgument('sourceDir', InputArgument::REQUIRED, 'The source directory containing the sculpin files'),
+                    new InputArgument('outputDir', InputArgument::REQUIRED, 'The directory where to write the files')
+                )
+            );
     }
 
     /**
@@ -30,5 +39,35 @@ class CrawlerCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $sourceDir = $input->getArgument('sourceDir');
+        $outputDir = $input->getArgument('outputDir');
+
+        $curDate = strtotime(date('Y-m-d'));
+        $usergroupRepository = new SculpinUsergroupRepository($sourceDir);
+        $usergroups = $usergroupRepository->getUsergroups();
+        foreach ($usergroups as $usergroup) {
+            /** @var Usergroup $usergroup */
+            $output->writeln(sprintf('Processing user group "%s"', $usergroup->getSlug()));
+
+            $eventRepository = RepositoryFactory::create($usergroup->getRepoType());
+            $usergroupEvents = $eventRepository->getEvents($usergroup->getEventLink());
+            $output->writeln(sprintf('Found "%s" events for user group "%s"', count($usergroupEvents), $usergroup->getSlug()));
+            foreach ($usergroupEvents as $usergroupEvent) {
+                /** @var Event $usergroupEvent */
+                if (strtotime($usergroupEvent->getDate()) >= $curDate) {
+                    // write event to disk in the format sculpin expects it
+                    $outputFile = $outputDir . '/' . $usergroupEvent->getDate() . '_' . $usergroup->getSlug() . '.md';
+                    $content = '---'. "\n";
+                    $content .= 'title: '.$usergroupEvent->getTitle(). "\n";
+                    $content .= 'date: '.$usergroupEvent->getDate(). "\n";
+                    $content .= 'location: '.$usergroupEvent->getLocation(). "\n";
+                    $content .= 'link: '.$usergroupEvent->getLink(). "\n";
+                    $content .= '---'. "\n";
+
+                    file_put_contents($outputFile, $content);
+                    $output->writeln(sprintf('Dumped event "%s" for user group "%s"', $usergroupEvent->getTitle(), $usergroup->getSlug()));
+                }
+            }
+        }
     }
 }
